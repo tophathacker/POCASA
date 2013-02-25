e059307660df87dd114d010a0e625d0111fcbcb5
tophathacker <tophathacker@gmail.com>
2013-02-25 14:58:05 -0800

    played with more python gui stuff

--- python_gui/win.py
+++ python_gui/win.py
@@ -10,6 +10,18 @@ class Base:
     print "Thanks for playing!"
     gtk.main_quit()
 
+  def handleButton(self, widget):
+    print widget.get_name()
+
+  def handleRegButton(self,widget):
+    if widget.get_label() == "0":
+      widget.set_label("1")
+    else:
+      widget.set_label("0")
+
+  def shiftIn(self, widget):
+    #do nothing for now
+
   def myhide(self, widget):
     if self.button1.get_visible() : 
       self.button1.hide()
@@ -24,15 +36,51 @@ class Base:
     self.window.set_size_request(500,500)
     self.window.set_title("Pocasa - Pi")
 
+    self.reg01 = gtk.Button("0")
+    self.reg01.connect("clicked", self.handleRegButton)
+
+
     self.button1 = gtk.Button("Exit")
     self.button1.connect("clicked", self.destroy)
     self.button1.set_tooltip_text("This button closes this window")
     self.button2 = gtk.Button("Toggle")
-    self.button2.connect("clicked", self.myhide)
+    self.button2.connect("clicked", self.handleButton)
+    self.button2.set_name("button2")
     self.exitLabel = gtk.Label("this will exit! -->")
 
     self.mainBox = gtk.VBox()
 
+    self.buttonBox = gtk.HBox()
+    self.labelBox = gtk.HBox()
+
+    for i in range(10):
+      button = gtk.Button("0")
+      button.set_name("reg_"+str(i))
+      button.connect("clicked",self.handleRegButton)
+      self.buttonBox.pack_start(button)
+      label = gtk.Label("0")
+      label.set_name("label_" + str(i))
+      self.labelBox.pack_start(label)
+
+    
+    self.reg_01 = gtk.Button("0")
+    self.reg_01.connect("clicked", self.handleRegButton)
+    self.buttonBox.pack_start(self.reg_01)
+    
+    self.reg_01 = gtk.Button("0")
+    self.reg_01.connect("clicked", self.handleRegButton)
+    self.buttonBox.pack_start(self.reg_01)
+    
+    self.reg_01 = gtk.Button("0")
+    self.reg_01.connect("clicked", self.handleRegButton)
+    self.buttonBox.pack_start(self.reg_01)
+    
+    self.reg_01 = gtk.Button("0")
+    self.reg_01.connect("clicked", self.handleRegButton)
+    self.buttonBox.pack_start(self.reg_01)
+    
+    self.mainBox.pack_start(self.buttonBox)
+    self.mainBox.pack_start(self.labelBox)
     self.box1 = gtk.HBox()
     self.box1.pack_start(self.exitLabel)
     self.box1.pack_start(self.button1)
