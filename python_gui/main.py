ec93d6fb4a7a1115779f77fe4909eb7479f9e096
Ryan Hatfield <tophathacker@gmail.com>
2013-03-02 17:54:34 -0600

    chagned to work on the pi with gtk3 and python2

--- python_gui/main.py
+++ python_gui/main.py
@@ -2,8 +2,8 @@
 
 from gi.repository import Gtk
 
-class POCASA_Pi(Gtk.Window):
-  
+class POCASA_Pi():
+ 
   def on_window1_destory(self,widget):
     Gtk.main_quit()
 
@@ -15,30 +15,29 @@ class POCASA_Pi(Gtk.Window):
     #  "on_window1_destroy":Gtk.main_quit()
     #}
     builder.connect_signals(self)
-
-    self.area = builder.get_object("area")
+    self.window = builder.get_object("window1")
     self.clicks = [];
      
   def destroy(widget,self):
     Gtk.main_quit()
 
   def on_area_press(self,widget,event):
-    print "on_area_press " , widget.get_name()
+    print "on_area_press " + widget.get_name()
     self.clicks.append([event.x,event.y])
-    self.area.queue_draw()
+    widget.queue_draw()
 
     return True
 
   def on_area_draw(self, drawing_area, cairo_context):
-    print "on_area_draw " , drawing_area.get_name()
+    print "on draw" ,drawing_area.get_name()
     cairo_context.move_to(50,50)
     for point in self.clicks:
       cairo_context.line_to(point[0],point[1])
     cairo_context.stroke()
 
   def manualChanged(self,widget):
-    print "manualChange: " , widget.get_text()
+    print widget.get_text()
 
 win = POCASA_Pi()
-win.show_all()
+win.window.show_all()
 Gtk.main()
